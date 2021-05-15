0630dc19ed24aab391933f1d71b1079c1c3918bb
Argem Flores <argemflores@gmail.com>
2021-05-15 15:44:32 +0800

    Answer exercises on strings

---
+++ sec3/exer3_StringIndexing.py
@@ -0,0 +1,7 @@
+# Write your string index below
+# Start with 'Hello World'
+# and make sure to match spaces and capitalization exactly
+
+# Return just the letter 'r' from 'Hello World' with only one line of code and without assigning the string to a variable
+
+'Hello World'[8]
---
+++ sec3/exer4_StringSlicing.py
@@ -0,0 +1,6 @@
+# Use string slicing to grab the word 'ink' from 'tinker'
+# For example, 'education'[3:6] returns 'cat'
+
+# Get the substring 'ink' from 'tinker using only one line of code and without assigning the string to a variable
+
+'tinker'[1:4]
