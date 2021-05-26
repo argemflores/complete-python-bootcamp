7b6ace13ea6783500e1c9091482721f4347f97aa
Argem Flores <argemflores@gmail.com>
2021-05-26 20:42:18 +0800

    Add more Warmup-1 practice exercises

---
+++ practice/codingbat/warmup1/warmup1_front_back.py
@@ -0,0 +1,13 @@
+# front_back
+
+# Given a string, return a new string where the first and last chars have been exchanged.
+
+# front_back('code') → 'eodc'
+# front_back('a') → 'a'
+# front_back('ab') → 'ba'
+
+def front_back(str):
+    if len(str) > 1:
+        return str[-1] + str[1:-1] + str[0]
+    else:
+        return str
