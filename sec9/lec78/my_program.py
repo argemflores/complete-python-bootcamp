1c582a9c3351103fc9c3eb04ad47854a75663ae7
Argem Flores <argemflores@gmail.com>
2021-07-19 19:46:02 +0800

    Create sample packages and modules
    __init__.py indicates a folder is a package
    Import methods from other Python scripts

---
+++ sec9/lec78/my_program.py
@@ -0,0 +1,8 @@
+from my_module import my_func
+from MyMainPackage import my_main_script
+from MyMainPackage.MySubPackage import my_sub_script
+
+my_func()
+
+my_main_script.main_report()
+my_sub_script.sub_report()
