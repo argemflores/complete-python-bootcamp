053f3ec4ed7b6e6dc9ed4d07036c36c64f4c2bfa
Argem Flores <argemflores@gmail.com>
2021-07-28 15:06:35 +0800

    Add comment to initialize Dealer

--- sec11/blackjack/Dealer.py
+++ sec11/blackjack/Dealer.py
@@ -6,4 +6,5 @@ Dealer (Person): the computer dealer
 class Dealer(Person):
     """Initialize object"""
     def __init__(self, name='Dealer'):
+        # initialize Dealer as Person
         Person.__init__(self, name)
