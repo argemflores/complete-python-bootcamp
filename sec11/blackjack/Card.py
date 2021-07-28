4d04d6247f857eab524324a157797dec8e7e31c0
Argem Flores <argemflores@gmail.com>
2021-07-28 15:17:29 +0800

    Move suits, ranks, and values from Deck to Card
    String details of card

--- sec11/blackjack/Card.py
+++ sec11/blackjack/Card.py
@@ -3,6 +3,11 @@ Card: playing card in a normal deck
 """
 
 class Card():
+    """Class attributes"""
+    suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+    ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+    values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': [1, 11]}
+    
     """Initialize object"""
     def __init__(self, suit, rank, value):
         # hearts, diamonds, spades, clubs
@@ -17,6 +22,6 @@ class Card():
         # Ace counts as 1 or 11 depending on the player's choice
         self.value = value
     
-    """Ask player to choose the value of the Ace card for their hand"""
-    def choose(self):
-        pass
+    """Return details about the card"""
+    def __str__(self):
+        return "{} of {}".format(self.rank, self.suit)
