9bb90a703c22eef00e2e09459ff7238d26ca80d1
Argem Flores <argemflores@gmail.com>
2021-07-28 15:18:10 +0800

    Add methods and update attributes of Deck
    deal_one(): draw top card
    string details of Deck
    length/no. of cards in Deck

--- sec11/blackjack/Deck.py
+++ sec11/blackjack/Deck.py
@@ -1,29 +1,28 @@
-from Card import Card
+from card import Card
 
 """
 Deck: normal collection of 52 cards
 """
 class Deck():
-    """Class attributes"""
-    suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-    ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-    values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': [1, 11]}
-    
     """Initialize class"""
     def __init__(self):
         # list of all 52 cards in a normal deck
         self.cards = []
         
         # fill up deck with the basic 52 cards
-        for suit in self.suits:
-            for rank in self.ranks:
-                card = Card(suit, rank, self.values[rank])
+        for suit in Card.suits:
+            for rank in Card.ranks:
+                card = Card(suit, rank, Card.values[rank])
                 self.cards.append(card)
     
-    """Draw a card from the deck then give to person's hand"""
-    def deal(self):
-        pass
+    """Draw the top card from the deck"""
+    def deal_one(self):
+        return self.cards.pop(-1)
     
-    """Get the total number of cards left in the deck"""
-    def count(self):
-        pass
+    """Return details about the deck"""
+    def __str__(self):
+        return "{} card/s in deck".format(len(self.cards))
+    
+    """Return the total number of cards left in the deck"""
+    def __len__(self):
+        return len(self.cards)
