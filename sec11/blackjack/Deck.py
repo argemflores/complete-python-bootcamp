410aa235163a61834bf1d33eb0ac66683fbc3954
Argem Flores <argemflores@gmail.com>
2021-07-29 18:51:58 +0800

    Add shuffle method and attribute to Deck class

--- sec11/blackjack/Deck.py
+++ sec11/blackjack/Deck.py
@@ -1,9 +1,13 @@
-from card import Card
+from Card import Card
+from random import shuffle
 
 """
 Deck: normal collection of 52 cards
 """
 class Deck():
+    # tells whether the deck is shuffled (default: False)
+    is_shuffled = False
+    
     """Initialize class"""
     def __init__(self):
         # list of all 52 cards in a normal deck
@@ -17,11 +21,16 @@ class Deck():
     
     """Draw the top card from the deck"""
     def deal_one(self):
-        return self.cards.pop(-1)
+        return self.cards.pop()
+    
+    """Shuffle the cards in the deck"""
+    def shuffle(self):
+        shuffle(self.cards)
+        self.is_shuffled = True
     
     """Return details about the deck"""
     def __str__(self):
-        return "{} card/s in deck".format(len(self.cards))
+        return "{} card/s in deck (shuffled: {})".format(len(self.cards), str(self.is_shuffled))
     
     """Return the total number of cards left in the deck"""
     def __len__(self):
