caa948a4a97ab2e8901928d1d713f3a0235a5c80
Argem Flores <argemflores@gmail.com>
2021-09-08 20:52:52 +0800

    Rename folder to 21.09

--- capstone/21.09.08/coin_flip_simulation.py
+++
@@ -1,1 +0,0 @@
-# Coin Flip Simulation - Write some code that simulates flipping a single coin however many times the user decides. The code should record the outcomes and count the number of tails and heads.
