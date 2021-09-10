ca9e43043ec769f4c246fa58824c6bf49affe0ce
Argem Flores <argemflores@gmail.com>
2021-09-10 21:21:26 +0800

    Create initial ciphers.py script

---
+++ capstone/21.09/ciphers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Vigenere / Vernam / Caesar Ciphers
+
+Functions for encrypting and decrypting data messages. Then send them to a friend.
+"""
+
+def main():
+    """
+    Main function
+
+    Executes the main flow of the script
+    """
+
+if __name__ == '__main__':
+    main()
